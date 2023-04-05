1890be76d44092b1b659aeb683ec3722aaef8d26
Carson Buttars <carsonbuttars13@gmail.com>
2023-04-05 01:04:43 -0600

    Adding final images

--- exploratory-data-analysis.py
+++ exploratory-data-analysis.py
@@ -4,7 +4,6 @@ import pandas as pd
 from plotnine import *
 from plotnine_prism import *
 import dataframe_image as dfi
-import cv2
 
 
 # Load data
@@ -12,8 +11,7 @@ df = pd.read_csv('homework/Stat_386/nba-draft-data/pick-data.csv')
 
 
 # Image path
-path = '~/homework/Stat_386/nba-draft-data/'
-
+path = 'homework/Stat_386/nba-draft-data/images/'
 
 # Making a theme template
 def custom_theme():
@@ -29,22 +27,18 @@ def custom_theme():
 ## Simple stats
 simple = df[['Pick','Years','Games','Minutes_pg']]
 simple['Minutes'] = simple.Games * simple.Minutes_pg
-simple = simple.groupby('Pick').mean().reset_index()
 
 ## Shooting stats
 shooting = df[['Pick','FieldGoal_pcent','ThreePoint_pcent','Freethrow_pcent']]
-shooting = shooting.groupby('Pick').mean().reset_index()
 
 ## Standard stats
 standard = df[['Pick','Points_pg','Rebounds_pg','Assists_pg']]
-standard = standard.groupby('Pick').mean().reset_index()
 
 ## Advanced stats
 advanced = df[['Pick','Winshares_p48','Box_plusminus','Value_over_replacement']]
-advanced = advanced.groupby('Pick').mean().reset_index()
 
 ## Aggregated Table
-table = df.groupby(pd.cut(df.Pick, np.arange(0,61,10))).mean().drop('Pick', axis = 1).reset_index().round(decimals = 2)
+table = df.groupby(pd.cut(df.Pick, np.arange(0,61,10))).mean().drop('Pick', axis = 1).reset_index().round(decimals = 2).drop('Unnamed: 0', axis = 1)
 table.Pick = ['1-10', '10-21', '21-30', '31-40', '41-50', '51-60']
 
 
@@ -53,27 +47,31 @@ table.Pick = ['1-10', '10-21', '21-30', '31-40', '41-50', '51-60']
 dfi.export(table, path + 'overall-comparison-table.png', table_conversion = 'selenium')
 
 ## Standard stats comparisons
-(ggplot(standard.groupby('Pick').mean().reset_index()) +
- geom_line(aes('Pick','Points_pg'), color = '#F71480') +
- labs(title = 'Points per Game by Pick', y = 'Points per game') +
+(ggplot(standard) +
+ geom_smooth(aes('Pick','Points_pg'), color = '#F71480') +
+ labs(title = 'Mean Points per Game by Pick', x = 'Pick', y = 'Points per game') +
  scale_color_prism(palette = 'candy_bright') +
  custom_theme()
  ).save(path + 'points_pg.png')
-(ggplot(standard.groupby('Pick').mean().reset_index()) +
- geom_line(aes('Pick','Rebounds_pg'), color = '#F71480') +
- labs(title = 'Rebounds per Game by Pick', y = 'Rebounds per game') +
+(ggplot(standard) +
+ geom_smooth(aes('Pick','Rebounds_pg'), color = '#F71480') +
+ labs(title = 'Mean Rebounds per Game by Pick', x = 'Pick', y = 'Rebounds per game') +
  scale_color_prism(palette = 'candy_bright') +
  custom_theme()
  ).save(path + 'rebounds_pg.png')
-(ggplot(standard.groupby('Pick').mean().reset_index()) +
- geom_line(aes('Pick','Assists_pg'), color = '#F71480') +
- labs(title = 'Assists per Game by Pick', y = 'Assists per game') +
+(ggplot(standard) +
+ geom_smooth(aes('Pick','Assists_pg'), color = '#F71480') +
+ labs(title = 'Mean Assists per Game by Pick', x = 'Pick', y = 'Assists per game') +
  scale_color_prism(palette = 'candy_bright') +
  custom_theme()
  ).save(path + 'assists_pg.png')
 
 ## Shooting Comparisons
-(ggplot(pd.melt(shooting, id_vars = ['Pick'])) +
+shots = pd.melt(shooting, id_vars = ['Pick'])
+shots.loc[shots.variable == 'FieldGoal_pcent', 'variable'] = 'Field Goal Percentage'
+shots.loc[shots.variable == 'ThreePoint_pcent', 'variable'] = 'Three Point Percentage'
+shots.loc[shots.variable == 'Freethrow_pcent', 'variable'] = 'Free Throw Percentage'
+(ggplot(shots) +
  geom_jitter(aes('Pick', 'value', color = 'variable'), alpha = .35) +
  geom_smooth(aes('Pick', 'value', color = 'variable')) +
  labs(title = 'Shooting Percentage by Pick', x = 'Pick', y = 'Shooting Percentage') +
@@ -85,24 +83,28 @@ dfi.export(table, path + 'overall-comparison-table.png', table_conversion = 'sel
 (ggplot(simple) +
  geom_density(aes('Games'), color = '#F71480') +
  custom_theme() +
- labs(title = 'Games Played') +
+ labs(title = 'Games Played', y = 'Frequency') +
  scale_color_prism(palette = 'candy_bright')
  ).save(path + 'games_density.png')
 (ggplot(simple) +
  geom_density(aes('Years'), color = '#F71480') +
  custom_theme() +
- labs(title = 'Years in the League') +
+ labs(title = 'Years in the League', y = 'Frequency') +
  scale_color_prism(palette = 'candy_bright')
  ).save(path + 'years_density.png')
 (ggplot(simple) +
  geom_density(aes('Minutes'), color = '#F71480') +
  custom_theme() +
- labs(title = 'Minutes Played') +
+ labs(title = 'Minutes Played', y = 'Frequency') +
  scale_color_prism(palette = 'candy_bright')
  ).save(path + 'minutes_density.png')
 
 ## Shooing Comparisons with minutes limit
-(ggplot(pd.melt(shooting.loc[simple.Minutes > 5000, :], id_vars = ['Pick'])) +
+shots = pd.melt(shooting.loc[simple.Minutes > 5000, :], id_vars = ['Pick'])
+shots.loc[shots.variable == 'FieldGoal_pcent', 'variable'] = 'Field Goal Percentage'
+shots.loc[shots.variable == 'ThreePoint_pcent', 'variable'] = 'Three Point Percentage'
+shots.loc[shots.variable == 'Freethrow_pcent', 'variable'] = 'Free Throw Percentage'
+(ggplot(shots) +
  geom_jitter(aes('Pick', 'value', color = 'variable'), alpha = .35) +
  geom_smooth(aes('Pick', 'value', color = 'variable')) +
  labs(title = 'Shooting Comparison by Pick with Minutes Floor') +
@@ -114,18 +116,19 @@ dfi.export(table, path + 'overall-comparison-table.png', table_conversion = 'sel
 (ggplot(advanced) +
  geom_smooth(aes('Pick','Winshares_p48'), color = '#F71480') +
  geom_jitter(aes('Pick','Winshares_p48'), alpha = .35, color = '#F71480') +
- labs(title = 'Winshares per 48 Minutes by Pick') +
+ labs(title = 'Winshares per 48 Minutes by Pick', y = 'Winshare per 48 Minutes') +
  custom_theme()
- ).save('winshares_p48.png')
+ ).save(path + 'winshares_p48.png')
 (ggplot(advanced) +
  geom_smooth(aes('Pick','Box_plusminus'), color = '#F71480') +
  geom_jitter(aes('Pick','Box_plusminus'), alpha = .35, color = '#F71480') +
- labs(title = 'Box Plus Minus by Pick') +
+ labs(title = 'Box Plus Minus by Pick', y = 'Box Plus Minus') +
+ ylim(-30, 10) +
  custom_theme()
- ).save('box_plusminus.png')
+ ).save(path + 'box_plusminus.png')
 (ggplot(advanced) +
  geom_smooth(aes('Pick','Value_over_replacement'), color = '#F71480') +
  geom_jitter(aes('Pick','Value_over_replacement'), alpha = .35, color = '#F71480') +
- labs(title = 'Value Over Replacement by Pick') +
+ labs(title = 'Value Over Replacement by Pick', y = 'Value Over Replacement') +
  custom_theme()
- ).save('value_over_replacement.png')
+ ).save(path + 'value_over_replacement.png')
--- joining-images.py
+++ joining-images.py
@@ -2,25 +2,25 @@
 import cv2
 
 # Image path
-path = '~/homework/Stat_386/nba-draft-data/'
+path = '/home/carson/homework/Stat_386/nba-draft-data/images/'
 
 # Standard Stats Comparisons
 img1 = cv2.imread(path + 'points_pg.webp')
 img2 = cv2.imread(path + 'rebounds_pg.webp')
 img3 = cv2.imread(path + 'assists_pg.png')
 result = cv2.hconcat([img1, img2, img3])
-cv2.imwrite(path + 'standard_comparison.webp')
+cv2.imwrite(path + 'final/standard_comparison.webp', result)
 
 # Basic Stats Comparisons
 img1 = cv2.imread(path + 'games_density.webp')
 img2 = cv2.imread(path + 'years_density.webp')
 img3 = cv2.imread(path + 'minutes_density.webp')
 result = cv2.hconcat([img1, img2, img3])
-cv2.imwrite(path + 'basic_comparison.webp')
+cv2.imwrite(path + 'final/basic_comparison.webp', result)
 
 # Advanced Stats Comparisons
 img1 = cv2.imread(path + 'winshares_p48.webp')
 img2 = cv2.imread(path + 'box_plusminus.webp')
 img3 = cv2.imread(path + 'value_over_replacement.webp')
 result = cv2.hconcat([img1, img2, img3])
-cv2.imwrite(path + 'advanced_comparison.webp')
+cv2.imwrite(path + 'final/advanced_comparison.webp', result)
